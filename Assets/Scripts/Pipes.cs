e65d5820c528748ee3bab7001cf404f6ad16d441
SumRandomPerson <mikegui343@gmail.com>
2023-04-24 09:11:36 -0400

    changes

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMovement : MonoBehaviour
+{
+    
+    public float speed = 3.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Move pipes left
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+
+        if (transform.position.magnitude > 20f)
+
+        {
+            Destroy(gameObject);
+        }
+    }
+}
